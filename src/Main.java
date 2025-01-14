d7621f516f9750546e3469351d282ca63907cd5d
Vishal17-11 <vishalchinnusamy11@gmail.com>
2025-01-14 22:59:12 +0530

    Chang: Main file

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,16 @@ class Main{
 
 
     public static void main(String[] args) {
+        for (int i = 1; i <=10 ; i++) {
 
+            if(i%2 == 0){
+//                System.out.println(i);
+                return;
+            }else{
+                System.out.println(i);
+                i++;
+            }
+
+        }
     }
 }
